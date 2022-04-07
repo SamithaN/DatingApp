e84f66c7026711f1d2d1103ad1c7be985b4688df
SamithaN <samithaneranjan23@gmail.com>
2022-04-07 11:25:14 +0530

    client logging and register

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  
+ 
+  constructor() { }
+
+  ngOnInit(): void {    
+    console.log(this.registerMode)
+  }
+
+  registerToggle(){
+    this.registerMode = !this.registerMode;
+    console.log(this.registerMode);
+  }
+
+  
+
+  cancelRegisterMode(event: boolean){
+    this.registerMode = event;
+  }
+
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { AccountService } from './../_services/account.service';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+//import { EventEmitter } from 'events';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() usersFromComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {}
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response =>{
+      console.log(response);
+      this.cancel();
+    },error=>{
+      console.log(error); 
+    }
+    )
+  }
+
+  cancel(){    
+    this.cancelRegister.emit(false); 
+  }
+}
